3e9aafd96ec32bcf4e10085c3445f57fc1f3c8bb
misalibaytb <misaliba@michlip.eu>
2024-04-20 14:30:40 +0200

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Check server.jar size
+if [ "$(du -b /home/container/server.jar | cut -f1)" -lt 1048576 ]; then
+    echo "Error: this server.jar is not allowed, only use minecraft jars." >&2
+    exit 1
+fi
+
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
+
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Switch to the container's working directory
+cd /home/container || exit 1
+
+# Print Java version
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mjava -version\n"
+java -version
+
+# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
+# variable format of "${VARIABLE}" before evaluating the string and automatically
+# replacing the values.
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+
+# Display the command we're running in the output, and then execute it with the env
+# from the container itself.
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
+# shellcheck disable=SC2086
+eval ${PARSED}
